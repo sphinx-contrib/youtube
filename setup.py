99004ac10f9616eb3b554c4090f1c7f6c7ee83d2
David Ham <David.Ham@imperial.ac.uk>
2021-09-05 20:46:12 +0100

    missing comma

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description=long_desc,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable'
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
