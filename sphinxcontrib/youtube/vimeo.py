8175c3a36a9c054bc8e2ce6216a6db091820690e
TunaLobster <charliepjohnson@gmail.com>
2021-09-11 15:05:18 -0500

    Remove unused functools from imports

--- sphinxcontrib/youtube/vimeo.py
+++ sphinxcontrib/youtube/vimeo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from . import utils
-from functools import partial
 
 
 class vimeo(utils.video):
--- sphinxcontrib/youtube/youtube.py
+++ sphinxcontrib/youtube/youtube.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from . import utils
-from functools import partial
 
 
 class youtube(utils.video):
