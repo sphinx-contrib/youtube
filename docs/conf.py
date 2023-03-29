0f119b0e7500e9ae6ef0de4a4d62ae62eb1179d2
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-03-29 09:52:50 +0200

    update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -7,13 +7,6 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup ----------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 from datetime import datetime
 
 from sphinxcontrib.youtube import __version__
@@ -23,38 +16,41 @@ from sphinxcontrib.youtube import __version__
 project = "sphinxcontrib-youtube"
 author = "David A. Ham, Chris Pickel and others"
 copyright = f"2011-{datetime.now().year}, {author}"
-
-# The full version, including alpha/beta/rc tags
 release = __version__
 
 # -- General configuration -----------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx_copybutton", "sphinxcontrib.youtube"]
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = ["sphinx_copybutton", "sphinxcontrib.youtube", "sphinx_design"]
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["**.ipynb_checkpoints"]
-
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
-
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "logo": {
+        "text": project,
+    },
+    "use_edit_page_button": True,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/12rambau/youtube",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "Pypi",
+            "url": "https://pypi.org/project/sphinxcontrib-youtube/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+}
+html_context = {
+    "github_user": "sphinx-contrib",
+    "github_repo": "youtube",
+    "github_version": "main",
+    "doc_path": "docs",
+}
 
 # -- Option for Latex output ---------------------------------------------------
 
---
+++ docs/index.rst
@@ -0,0 +1,44 @@
+:html_theme.sidebar_secondary.remove:
+
+sphinxcontrib-youtube
+=====================
+
+.. toctree::
+   :hidden:
+
+   usage
+   contribute
+
+Overview
+--------
+
+This module provides support for including YouTube and Vimeo videos in Sphinx rst documents.
+
+This module defines directives, youtube and vimeo which insert videos from the respective platforms. They take a single, required argument, which is the video ID:
+
+.. code-block:: rst
+
+    ..  youtube:: dQw4w9WgXcQ
+
+..  youtube:: dQw4w9WgXcQ
+
+Documentation contents
+----------------------
+
+The documentation contains 2 main sections:
+
+.. grid:: 1 2 2 2
+
+   .. grid-item::
+
+      .. card:: Getting started
+         :link: usage.html
+
+         Usage and installation
+
+   .. grid-item::
+
+      .. card:: Contribute
+         :link: contribute.html
+
+         Help us improve the lib.
