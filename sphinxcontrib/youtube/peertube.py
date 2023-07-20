3694585d74393ebfdcf7a53e5eefcaed0ae4df49
Martin Marmsoler <martin.marmsoler@gmail.com>
2023-07-20 18:41:46 +0200

    missing else path

--- sphinxcontrib/youtube/peertube.py
+++ sphinxcontrib/youtube/peertube.py
@@ -34,7 +34,9 @@ class PeerTube(utils.Video):
 
 def visit_peertube_node(self, node):
     """Custom html visit node."""
-    if "server" not in node:
+    if "server" in node:
+        server = node["server"]
+    else:
         server = "peertube.tv"  # default
     node["platform_url"] = f"https://{server}/videos/embed/"
     return utils.visit_video_node_html(self, node)
